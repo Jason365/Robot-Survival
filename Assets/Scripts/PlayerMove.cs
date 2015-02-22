33604a8e7c915cbd7bed8b9da3690103fecc466b
Jason365 <brestel.jason@gmail.com>
2015-02-22 15:12:16 -0700

    Made my version of Movement

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	
+	public float maxSpeed = 2f;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+		float xtrans = 0;
+		float ytrans = 0;
+		
+		
+		xtrans = Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+		
+		ytrans = Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
+		
+		transform.Translate (-xtrans, 0f, -xtrans);
+		transform.Translate (-ytrans, 0f, ytrans);
+		
+		
+	}
+}
