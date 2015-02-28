a9b31424ff71b3e9c1184a6b3128ce3dbda089f4
Jason365 <brestel.jason@gmail.com>
2015-02-27 20:05:46 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Library/InspectorExpandedItems.asset
    	Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll
    	Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll.mdb
    	Library/ScriptAssemblies/Assembly-CSharp.dll
    	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	Library/ScriptAssemblies/Assembly-UnityScript-firstpass.dll
    	Library/ScriptAssemblies/Assembly-UnityScript-firstpass.dll.mdb
    	Library/assetDatabase3
    	Library/metadata/3e/3e044b8a3f8df1d43bfb989bce17460c
    
    What even

--- Assets/Scripts/MouseMove.cs
+++ Assets/Scripts/MouseMove.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class MouseMove : MonoBehaviour {
 	public Ray cameraRay;                // The ray that is cast from the camera to the mouse position
 	public RaycastHit cameraRayHit;    // The object that the ray hits
-	
+	float lockPos = -90;
+	float lockPos2 = 0;
+
+
 	void Update () 
 	{
+
+		transform.rotation = Quaternion.Euler (lockPos, lockPos2, 0);
 		// Cast a ray from the camera to the mouse cursor
 		cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 		
@@ -16,8 +21,10 @@ public class MouseMove : MonoBehaviour {
 			// ...and if that object is the ground...
 			if(cameraRayHit.transform.tag=="Ground")
 			{
+
 				// ...make the cube rotate (only on the Y axis) to face the ray hit's position 
-				Vector3 targetPosition = new Vector3(cameraRayHit.point.x, transform.position.y, cameraRayHit.point.z);
+				Vector2 targetPosition = new Vector2(cameraRayHit.point.x,
+				                                     cameraRayHit.point.y);
 				transform.LookAt(targetPosition);
 			}
 		}
